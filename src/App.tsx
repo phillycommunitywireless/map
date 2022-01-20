8bfecff457d4953d20facace52802b0b5db8d97b
Addie <addiebarronemail@gmail.com>
2022-01-20 16:34:48 -0500

    Mapbox tiles added

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import './App.css';
 
 function App() {
   // Center of Norris Square Park
-  const position: LatLngTuple = [39.98265, -75.1347];
+  const center: LatLngTuple = [39.98265, -75.1347];
+
+  const mapboxToken: string|undefined = process.env.REACT_APP_MAPBOX_TOKEN;
+  const mapboxUsername: string|undefined = process.env.REACT_APP_MAPBOX_USERNAME;
+  const mapboxStyle: string|undefined = process.env.REACT_APP_MAPBOX_STYLE;
 
   return (
-    <MapContainer center={position} zoom={16.5} scrollWheelZoom={true}>
+    <MapContainer center={center} zoom={16.5} scrollWheelZoom={true}>
       <TileLayer
-        url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`}
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+        maxZoom={18}
+        id={`${mapboxUsername}/${mapboxStyle}`}
+        tileSize={512}
+        zoomOffset={-1}
+        accessToken={mapboxToken}
       />
-      <Marker position={position}>
+      <Marker position={center}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
