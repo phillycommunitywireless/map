8649abfea61332c1730717eea777e97dedfb6aed
Addie <addiebarronemail@gmail.com>
2022-02-14 16:08:04 -0500

    Integrate review; add nodeType field

--- src/components/Map.types.ts
+++ src/components/Map.types.ts
@@ -1,25 +1,26 @@
 export interface Node {
   // This could be a MAC address, an installation ID we use internally, or a UUID generated just for this code
-  id: string|number,
+  nodeId: string|number,
+  // The internal type of this node: only "mesh" or "hub" for now.
+  nodeType: string,
   // User-facing label
   label?: string,
   // Lat/long
   lat: number,
   long: number,
-  // Direction the antenna is facing. How do we calculate these during install 
-  // and record it in a standard way? What do maps people use for angles
+  // Direction the antenna is facing, expressed as a compass heading with 0/360=North, 90=East, etc.
   direction: number,
-  // Information about device installed here
+  // Information about device installed at this node
   device: Device,
-  // Other nodes that are meshed with this one, either a relation or ID
+  // Other nodes that are meshed with this one, either by a relation or passed nodeId
   meshedWith: Node[]|(string[]|number[]),
 }
 
 export interface Device {
   // Name of the device
   name: string,
-  // Signal range (angle)
-  angleRange: number,
-  // Signal range (distance)
-  distanceRange: number,
+  // Signal range - angle of coverage
+  signalRangeAngle: number,
+  // Signal range - distance out from the antenna
+  signalRangeDistance: number,
 }
