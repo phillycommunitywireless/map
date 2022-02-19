2170ee0fb70ba7a96a14d4094b50eec4fb75f749
Addie <addiebarronemail@gmail.com>
2022-02-19 11:12:54 -0500

    Fix replace bug in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
             spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
             credentials: {
               client_email: process.env.GOOGLE_CLIENT_EMAIL,
-              private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+              private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
             }
         }
     },
